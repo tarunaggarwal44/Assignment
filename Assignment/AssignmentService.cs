b7087b658a7b237a891fab45fe70b5499eba60c2
tarun aggarwal <tarun.aggarwal4@gmail.com>
2021-01-30 21:16:23 +0400

    Assignment

---
+++ Assignment/AssignmentService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class AssignmentService
+    {
+        private readonly HttpClient client = new HttpClient();
+        public AssignmentService()
+        {
+            client.BaseAddress = new Uri("https://api.fda.gov/food/enforcement.json?search=report_date:%5b20040101+TO+20131231%5d&limit=1");
+        }
+
+
+        public async Task<string> GetResponse()
+        {
+            HttpResponseMessage response = await client.GetAsync(string.Empty);
+            if (response.IsSuccessStatusCode)
+            {
+                var responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            return string.Empty;
+        }
+    }
+}
---
+++ Assignment/Controllers/AssignmentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Assignment.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AssignmentController : ControllerBase
+    {
+
+        [HttpGet()]
+        public async Task<IActionResult> Get()
+        {
+            AssignmentService assignmentService = new AssignmentService();
+            var response = await assignmentService.GetResponse();
+
+            return this.Ok(response);
+        }
+
+     
+    }
+}
